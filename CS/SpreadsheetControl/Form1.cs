f6590e8002fc34190fed56b80e2919a69b2d5eb7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpreadsheetControl/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.Spreadsheet;
+using System.Diagnostics;
+
+namespace ConditionalFormatting_Examples
+{
+    public partial class Form1 : Form
+    {
+
+        IWorkbook workbook;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //  Access a workbook.
+            workbook = spreadsheetControl1.Document;
+            InitTreeListControl();
+
+        }
+
+        private void InitTreeListControl()
+        {
+            GroupsOfSpreadsheetExamples examples = new GroupsOfSpreadsheetExamples();
+            InitData(examples);
+            DataBinding(examples);
+        }
+
+        private void InitData(GroupsOfSpreadsheetExamples examples)
+        {
+            #region GroupNodes
+            examples.Add(new SpreadsheetNode("Conditional Formatting Examples"));
+            #endregion
+
+            #region ExampleNodes
+            // Add nodes to the "Conditional Formatting" group of examples.
+            examples[0].Groups.Add(new SpreadsheetExample("Format values that are above or below average", ConditionalFormatting.AddAverageConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells that are not between two specified values", ConditionalFormatting.AddRangeConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format top ranked values", ConditionalFormatting.AddRankConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells that contain the given text", ConditionalFormatting.AddTextConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format only unique values", ConditionalFormatting.AddSpecialConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format today's date", ConditionalFormatting.AddTimePeriodConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format values that are greater than a specified value", ConditionalFormatting.AddExpressionConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Use a formula to determine which cells to format", ConditionalFormatting.AddFormulaExpressionConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells by using a two-color scale", ConditionalFormatting.AddColorScale2ConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells by using a three-color scale", ConditionalFormatting.AddColorScale3ConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells by using data bars", ConditionalFormatting.AddDataBarConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Format cells by using a custom icon set", ConditionalFormatting.AddIconSetConditionalFormattingAction));
+            examples[0].Groups.Add(new SpreadsheetExample("Apply conditional formatting to a complex range", ConditionalFormatting.AddComplexRangeConditionalFormattingAction));
+            #endregion
+        }
+
+        void DataBinding(GroupsOfSpreadsheetExamples examples)
+        {
+            treeList1.DataSource = examples;
+            treeList1.ExpandAll();
+            treeList1.BestFitColumns();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadDocumentFromFile();
+            SpreadsheetExample example = treeList1.GetDataRecordByNode(treeList1.FocusedNode) as SpreadsheetExample;
+            if (example == null)
+                return;
+            Action<IWorkbook> action = example.Action;
+            action(workbook);
+            SaveDocumentToFile();
+        }
+
+        // ------------------- Load and Save a Document -------------------
+        private void LoadDocumentFromFile() {
+            #region #LoadDocumentFromFile
+            // Load a workbook from a file.
+            workbook.LoadDocument("Documents\\Document.xlsx", DocumentFormat.OpenXml);
+            #endregion #LoadDocumentFromFile
+        }
+
+        private void SaveDocumentToFile() {
+            #region #SaveDocumentToFile
+            // Save the modified document to a file.
+            workbook.SaveDocument("Documents\\SavedDocument.xlsx", DocumentFormat.OpenXml);
+            #endregion #SaveDocumentToFile
+        }
+    }
+}
---
+++ VB/SpreadsheetControl/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.Spreadsheet
+Imports System.Diagnostics
+
+Namespace ConditionalFormatting_Examples
+	Partial Public Class Form1
+		Inherits Form
+
+		Private workbook As IWorkbook
+
+		Public Sub New()
+			InitializeComponent()
+
+			'  Access a workbook.
+			workbook = spreadsheetControl1.Document
+			InitTreeListControl()
+
+		End Sub
+
+		Private Sub InitTreeListControl()
+			Dim examples As New GroupsOfSpreadsheetExamples()
+			InitData(examples)
+			DataBinding(examples)
+		End Sub
+
+		Private Sub InitData(ByVal examples As GroupsOfSpreadsheetExamples)
+'			#Region "GroupNodes"
+			examples.Add(New SpreadsheetNode("Conditional Formatting Examples"))
+'			#End Region
+
+'			#Region "ExampleNodes"
+			' Add nodes to the "Conditional Formatting" group of examples.
+			examples(0).Groups.Add(New SpreadsheetExample("Format values that are above or below average", ConditionalFormatting.AddAverageConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format cells that are not between two specified values", ConditionalFormatting.AddRangeConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format top ranked values", ConditionalFormatting.AddRankConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format cells that contain the given text", ConditionalFormatting.AddTextConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format only unique values", ConditionalFormatting.AddSpecialConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format today's date", ConditionalFormatting.AddTimePeriodConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format values that are greater than a specified value", ConditionalFormatting.AddExpressionConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Use a formula to determine which cells to format", ConditionalFormatting.AddFormulaExpressionConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format cells by using a two-color scale", ConditionalFormatting.AddColorScale2ConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format cells by using a three-color scale", ConditionalFormatting.AddColorScale3ConditionalFormattingAction))
+			examples(0).Groups.Add(New SpreadsheetExample("Format cells by using data bars", ConditionalFormatting.AddDataBarConditionalFormattingAction))
+            examples(0).Groups.Add(New SpreadsheetExample("Format cells by using a custom icon set", ConditionalFormatting.AddIconSetConditionalFormattingAction))
+            examples(0).Groups.Add(New SpreadsheetExample("Apply conditional formatting to a complex range", ConditionalFormatting.AddComplexRangeConditionalFormattingAction))
+'			#End Region
+		End Sub
+
+		Private Sub DataBinding(ByVal examples As GroupsOfSpreadsheetExamples)
+			treeList1.DataSource = examples
+			treeList1.ExpandAll()
+			treeList1.BestFitColumns()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			LoadDocumentFromFile()
+			Dim example As SpreadsheetExample = TryCast(treeList1.GetDataRecordByNode(treeList1.FocusedNode), SpreadsheetExample)
+			If example Is Nothing Then
+				Return
+			End If
+			Dim action As Action(Of IWorkbook) = example.Action
+			action(workbook)
+			SaveDocumentToFile()
+		End Sub
+
+		' ------------------- Load and Save a Document -------------------
+		Private Sub LoadDocumentFromFile()
+'			#Region "#LoadDocumentFromFile"
+			' Load a workbook from a file.
+			workbook.LoadDocument("Documents\Document.xlsx", DocumentFormat.OpenXml)
+'			#End Region ' #LoadDocumentFromFile
+		End Sub
+
+		Private Sub SaveDocumentToFile()
+'			#Region "#SaveDocumentToFile"
+			' Save the modified document to a file.
+			workbook.SaveDocument("Documents\SavedDocument.xlsx", DocumentFormat.OpenXml)
+'			#End Region ' #SaveDocumentToFile
+		End Sub
+	End Class
+End Namespace
